3c45a767605c7b5379b6df25b57696885bba96f4
Takkiii <s.takkiii.com@gmail.com>
2021-12-15 00:18:28 +0900

    Apply react-div-100vh and react-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  // other next config
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public'
+  }
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next.js PWA Example</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
